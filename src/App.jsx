791da6cdcb14321f1b0108a67bc3fe291a8a44a9
wallondo <wallondolaila@gmail.com>
2025-04-23 02:58:51 +0000

    2x

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+import react from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='App'>
+            <header>
+                <div>
+                  <span><img src="/cr.png" alt="" /></span>
+                  <span><img src="/cv.png" alt="" /></span>
+                </div>
+                <ol>
+                  <li><a href="#">Contactar Tutor</a></li>
+                  <li><a href="#">Questões</a></li>
+                  <li className='a4'>Ebook</li>
+                </ol>
+            </header>
+            <main>
+              <h1>Quer aprender Forex na prática do completo Zero e começar a lucrar de verdade?</h1>
+              <h3>"Oportunidade única para quem quer dominar o Forex em Angola! Aprenda cara a cara com quem vive o mercado"</h3>
+                <article className='art1'>
+                    <h1>Módulo 1</h1>
+                    <h3>Fundamentos do Trading</h3>
+                    <h5> Objetivo: Apresentar os conceitos básicos e preparar o iniciante para o ambiente do mercado</h5>
+                    <ol>
+                      <li>- O que é Trading? Tipos de Mercado (Forex, Cripto, Ações, etc.)</li>
+                      <li>- Glossário Essencial (Pips, Spread, Alavancagem, Stop Loss, Take Profit)</li>
+                      <li>- Tipos de Gráficos (de linhas, barras, velas japonesas)</li>
+                      <li>- Funcionamento do Forex e pares de moedas</li>
+                      <li>- Como funcionam as corretoras e plataformas (MT4/MT5, Quotex, etc.)</li>
+                    </ol>
+                    <a href="https://wa.me/244925786916?text='Saudações, Gostaria de começar com Módulo 1'" target="_blank" rel="noopener noreferrer">Solicitar +7.000.00.AOA</a>
+                </article>
+                <article className='art2'>
+                <h1>Módulo 2</h1>
+                    <h3>Leitura de Gráficos e Análise Técnica</h3>
+                      <h5>Objetivo: Ensinar o aluno a interpretar o mercado visualmente e tomar decisões com base em dados.</h5>
+                    <ol>
+                      <li>- Introdução à Análise Técnica</li>
+                      <li>- Suporte e Resistência</li>
+                      <li>- Tendências e Canal de Preço</li>
+                      <li>- Padrões de Velas (Doji, Martelo, Engolfo, etc.)</li>
+                      <li>- Indicadores básicos (RSI, MACD, Médias Móveis)</li>
+                      <li>- Timeframes e escolha do melhor tempo gráfico</li>
+                    </ol>
+                    <a href="https://wa.me/244925786916?text=Saudações, Gostaria de começar com Módulo 2" target="_blank" rel="noopener noreferrer">Solicitar  +10.000.00.AOA</a>
+                </article>
+                <article className='art3'>
+                <h1>Módulo 3</h1>
+                    <h3>- Gestão e Estratégias de Trading</h3>
+                      <h5>Objetivo: Preparar o aluno para aplicar com segurança seus conhecimentos no mercado real.</h5>
+                    <ol>
+                      <li>- Gestão de Risco (quanto arriscar por operação, Stop Loss inteligente)</li>
+                      <li>- Psicologia do Trader (controle emocional, disciplina)</li>
+                      <li>- Estratégias Simples para Iniciantes</li>
+                      <li>- Planejamento de Trades (diário de operações)</li>
+                      <li>- Erros comuns de iniciantes e como evitar</li>
+                      <li>- Corretoras de Forex confiáveis</li>
+                      <li>- Levantamentos e Depositos</li>
+                    </ol>
+                    <a href="https://wa.me/244925786916?text=Saudações, Gostaria de começar com Módulo 3" target="_blank" rel="noopener noreferrer">Solicitar  +12.000.00.AOA</a>
+                </article>
+                <section>
+                  <article className='art4' id='art4'>
+                  <h1>Módulo 4</h1>
+                      <h3>- Análise Relativa no Trading</h3>
+                        <h5>Objetivo: Ensinar como comparar ativos, moedas e movimentos de mercado para encontrar as melhores oportunidades.</h5>
+                      <ol>
+                        <li>- O que é Análise Relativa?</li>
+                        <li>- Correlação entre Pares de Moeda</li>
+                        <li>- Força Relativa entre Moedas</li>
+                        <li>- Indicadores de Análise Relativa</li>
+                        <li>- Estratégias com Análise Relativa</li>
+                        <li>- depositos e saques em Angola</li>
+                      </ol>
+                          <h3>Apenas com o término dos três módulos iniciais</h3>
+                      <button className='a4' disable>Solicitar  +17.000.00.AOA</button>
+                  </article>
+                </section>
+                <details>
+                <summary>
+                      Questões Constantes.
+                </summary>
+                    <h2>
+                      <span>Pouco Capital?</span> Poucas condições? Fins de semana? <span>Sem tempo? </span>Local ?! Confivel ? Vale a pena? Duração ?
+                      Pode ser fora de casa? <span>Pode ser ao domicílio?</span> Ar livre?! E Se Não Ter Uma Boa Didática? <span>Se eu não aprender? Há reenbolso?</span> Quanto tempo até eu poder entrar no mercado? Semana? Mês? Dias?
+                    </h2>
+                    <br />
+                    <h3>Fale direitamente com o Tutor e vé por ti mesmo!</h3>
+                </details>
+            </main>
+            
+      </div>
+  )
+}
+
+export default App
